a72337328eb91aaa5bd6c393d8cfbfdef1c3f294
isellar <ian@sellarfamily.com>
2025-11-30 10:25:43 -0800

    containerize and try to fix hang

---
+++ docker-run.ps1
@@ -0,0 +1,20 @@
+# PowerShell script to run Kaggle notebook environment locally with Docker
+# Run this from PowerShell
+
+# Set your Kaggle API credentials (you'll need to get these from Kaggle)
+$env:KAGGLE_USERNAME = "your_username"
+$env:KAGGLE_KEY = "your_api_key"
+
+# Get current directory
+$currentDir = (Get-Location).Path
+
+# Run the Kaggle Docker image
+docker run -it --rm `
+  -p 8888:8888 `
+  -v "${currentDir}:/home/jovyan/work" `
+  -e KAGGLE_USERNAME="$env:KAGGLE_USERNAME" `
+  -e KAGGLE_KEY="$env:KAGGLE_KEY" `
+  -e JUPYTER_ENABLE_LAB=yes `
+  gcr.io/kaggle-images/python:latest `
+  start.sh jupyter lab --NotebookApp.token='' --NotebookApp.password='' --ip=0.0.0.0 --port=8888
+
---
+++ docker-run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Script to run Kaggle notebook environment locally with Docker
+
+# Set your Kaggle API credentials (you'll need to get these from Kaggle)
+export KAGGLE_USERNAME="your_username"
+export KAGGLE_KEY="your_api_key"
+
+# Run the Kaggle Docker image
+# This uses the official Kaggle Docker image
+docker run -it --rm \
+  -p 8888:8888 \
+  -v "$(pwd):/home/jovyan/work" \
+  -e KAGGLE_USERNAME="$KAGGLE_USERNAME" \
+  -e KAGGLE_KEY="$KAGGLE_KEY" \
+  -e JUPYTER_ENABLE_LAB=yes \
+  gcr.io/kaggle-images/python:latest \
+  start.sh jupyter lab --NotebookApp.token='' --NotebookApp.password='' --ip=0.0.0.0 --port=8888
+
